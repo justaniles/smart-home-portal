e0576537d77507f5c44360442ae6f667ccf690d4
Justin Niles <justin.niles12@gmail.com>
2016-05-19 22:06:38 -0700

    Add shared directory

---
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'sd-toolbar',
+    templateUrl: 'app/shared/toolbar/toolbar.component.html',
+    styleUrls: ['app/shared/toolbar/toolbar.component.scss']
+})
+/**
+ * This class represents the toolbar component.
+ */
+export class ToolbarComponent {
+}
---
+++ src/app/shared/user/users.service.ts
@@ -0,0 +1,14 @@
+/**
+ * This class provides the NameList service with methods to
+ * read names and add names.
+ */
+export class UserService {
+
+    loginUser(email: string, password: string): void {
+        console.log(`user: ${email} password: ${password}`);
+    }
+
+    logoutUser(email: string) {
+
+    }
+}
