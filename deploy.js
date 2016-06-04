6161abf8447a31887ad895f08721f60fb53935a4
Justin Niles <justin.niles12@gmail.com>
2016-06-04 14:09:01 -0700

    Not updating this for a long-ass time

--- deploy.js
+++ deploy.js
@@ -3,16 +3,12 @@
 
 const ghpages = require("gh-pages");
 const path = require("path");
+const cp = require("child_process");
 const yargs = require("yargs");
 const argv = yargs.options({
         "b": {
             alias: "build",
             describe: "Build the project using prod build settings",
-            boolean: true
-        },
-        "d": {
-            alias: "deploy",
-            describe: "Deploy the project to gh-pages",
             boolean: true
         },
         "q": {
@@ -27,37 +23,29 @@ const argv = yargs.options({
 
 // Local variables:
 let runBuild = argv.b;
-let runDeploy = argv.d;
 let quiet = argv.q;
-
-if (!(runBuild || runDeploy)) {
-    yargs.showHelp();
-    process.exit();
-}
 
 if (runBuild) {
     console.log("Building for production...");
     runCommand("npm", ["run", "build:prod"]);
 }
 
-if (runDeploy) {
-    console.log("Deploying to gh-pages...");
-    ghpages.publish(path.join(__dirname, "dist"),
-        {
-            message: "Auto-generated commit",
-            logger: function(message) {
-                console.log(message);
-            }
-        },
-        function(err) {
-            if (err) {
-                console.error(`Finished with errors:\n${err}`);
-                return;
-            }
-            console.log("Done!");
+console.log("\nDeploying to gh-pages...");
+ghpages.publish(path.join(__dirname, "dist"),
+    {
+        message: "Deployment script commit",
+        logger: function(message) {
+            console.log(message);
         }
-    );
-}
+    },
+    function(err) {
+        if (err) {
+            console.error(`Finished with errors:\n${err}`);
+            return;
+        }
+        console.log("Done!");
+    }
+);
 
 // Helper functions:
 function runCommand(command, args) {
